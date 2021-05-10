0cabd416be5b2093d0230cbe7d76224d8d79c059
Arraying <paul.huebner@googlemail.com>
2021-05-10 15:10:37 +0200

    Cleaned up OAuth2 handler with await.

--- src/routes/oauth_authorized.js
+++ src/routes/oauth_authorized.js
@@ -4,77 +4,52 @@ const database = require('../database');
 // Administrator or can manage guild.
 const permissions = 0x00000020;
 
-module.exports = (req, res) => {
-    let code = req.query.code;
-    // Request the OAuth2 token from the code given in the callback.
-    client
-    .tokenRequest({
-        scope: ['identify', 'guilds'],
-        grantType: 'authorization_code',
-        code: code
-    })
-    .then(auth => {
+module.exports = handle;
+
+async function handle(req, res) {
+    try {
+        let code = req.query.code;
+        let auth = await client.tokenRequest({
+            scope: ['identify', 'guilds'],
+            grantType: 'authorization_code',
+            code: code
+        });
         // Obtained the token, need to fetch user information and all guilds.
         let tokenValue = auth.access_token;
         req.session.token = tokenValue;
-        // Combine both promises to make handling easier.
-        Promise
-        .all([client.getUser(tokenValue), client.getUserGuilds(tokenValue)])
-        .then(data => {
-            let user = data[0];
-            let guilds = data[1];
-            req.session.user = user;
-            // Only allow the guilds in which the user has permissions.
-            let permGuilds = guilds
-                // Sort alphabetically first.
-                .sort((a, b) => {
-                    return a.name.localeCompare(b.name);
-                })
-                // Filter.
-                .filter(guild => {
-                    if (guild.owner) {
-                        return true;
-                    }
-                    // Bitwise AND should determine if the permission bit is set.
-                    let numberPerms = parseInt(guild.permissions);
-                    if ((numberPerms & permissions) == permissions) {
-                        return true;
-                    }
-                    return false;
-                });
-            // Now check, of those, which are in the database.
-            database.getKnownGuilds(permGuilds.map(guild => guild.id))
-            .then(finalGuilds => {
-                // Only include the guilds in the database.
-                req.session.guilds = permGuilds.filter(guild => finalGuilds.includes(guild.id));
-                // Have an authorized parameter with just the IDs.
-                req.session.authorized = req.session.guilds.map(guild => guild.id);
-                redirect(req, res, '/panel/');
+        // Get the user + guilds.
+        let user = await client.getUser(tokenValue);
+        let guilds = await client.getUserGuilds(tokenValue);
+        req.session.user = user;
+        // Only allow the guilds in which the user has permissions.
+        let permGuilds = guilds
+            // Sort alphabetically first.
+            .sort((a, b) => {
+                return a.name.localeCompare(b.name);
             })
-            .catch(err => {
-                console.log(err.stack);
-                // We don't need to render the login because the login failed.
-                res.render('error', {
-                    title: 'SQL Error',
-                    subtitle: 'Could not retrieve guilds.'
-                });
+            // Filter.
+            .filter(guild => {
+                if (guild.owner) {
+                    return true;
+                }
+                // Bitwise AND should determine if the permission bit is set.
+                let numberPerms = parseInt(guild.permissions);
+                if ((numberPerms & permissions) == permissions) {
+                    return true;
+                }
+                return false;
             });
-        })
-        .catch(err => {
-            console.log(err.stack);
-            // We don't need to render the login because the login failed.
-            res.render('error', {
-                title: 'Discord Error',
-                subtitle: 'Could not retrieve data.'
-            });
-        });
-    })
-    .catch(err => {
-        console.log(err.stack);
-        // We don't need to render the login here either.
+        let known = await database.getKnownGuilds(permGuilds.map(guild => guild.id));
+        // Only include the guilds in the database.
+        req.session.guilds = permGuilds.filter(guild => known.includes(guild.id));
+        // Have an authorized parameter with just the IDs.
+        req.session.authorized = req.session.guilds.map(guild => guild.id);
+        redirect(req, res, '/select/');
+    } catch (exception) {
+        console.error(exception);
         res.render('error', {
-            title: 'Discord Error',
-            subtitle: 'Could not retrieve token.'
+            title: 'Authorization Error',
+            subtitle: 'Something went wrong. Please contact an administrator.'
         });
-    });
-};
+    }
+}
